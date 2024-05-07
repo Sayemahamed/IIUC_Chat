458ab374586742ebe55d6ccca4277fa7a12fcda6
Sayem Ahamed <95583680+Sayemahamed@users.noreply.github.com>
2024-05-07 21:10:55 +0600

    implemented authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
 import { Routes, Route } from "react-router-dom";
+import { auth } from "./firebase/config";
+import {
+  GoogleAuthProvider,
+  signInWithRedirect,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { useEffect } from "react";
 const App = () => {
+  useEffect(() => {
+    const getAuth = async () => {
+      await signInWithRedirect(auth, new GoogleAuthProvider());
+    };
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log(user);
+      } else {
+        getAuth();
+      }
+    });
+  }, []);
   return (
     <Routes>
       <Route path="/" element={<div>App</div>} />
