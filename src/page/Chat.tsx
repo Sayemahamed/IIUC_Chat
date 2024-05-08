8f88470ff200d5d5293a70d181b72dfb673ce5fa
Sayem Ahamed <95583680+Sayemahamed@users.noreply.github.com>
2024-05-09 00:05:05 +0600

    trying UI on mobile

--- src/page/Chat.tsx
+++ src/page/Chat.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { database } from "../firebase/config";
 import { get, ref } from "firebase/database";
-import { Avatar, Card, CardHeader, Grid } from "@mui/material";
+import { Avatar, Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
 
 interface userType {
   name: string;
@@ -47,6 +47,11 @@ const Chat = ({ userID, chatNode }: { userID: string; chatNode: string }) => {
             }
             title={userData.name}
           ></CardHeader>
+          <CardContent>
+            <Typography variant="body2" color={"GrayText"}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, repellendus dolores sit quis vel voluptate dicta consectetur in rerum officiis?
+            </Typography>
+          </CardContent>
         </Card>
       </Grid>
     </Grid>
