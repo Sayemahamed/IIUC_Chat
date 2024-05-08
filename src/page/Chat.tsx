121ec5c2995b7883859f595d304777a2e1b58e36
Sayem Ahamed <95583680+Sayemahamed@users.noreply.github.com>
2024-05-09 02:56:33 +0600

    going to sleep

--- src/page/Chat.tsx
+++ src/page/Chat.tsx
@@ -75,7 +75,7 @@ const Chat = ({ userID, chatNode }: { userID: string; chatNode: string }) => {
         }}
         multiline
         fullWidth
-        sx={{  position: "fixed", bottom: "0", my: 2, mx: "auto" }}
+        sx={{ position: "fixed", bottom: "0", my: 2, mx: "auto" }}
         focused={chatData !== ""}
         value={chatData}
         id="standard-basic"
