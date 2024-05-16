cee59cb03fbe33daa17e9c624ab43d126abd067c
Sayem Ahamed <95583680+Sayemahamed@users.noreply.github.com>
2024-05-16 21:03:46 +0600

    chat bar position corrected

--- src/page/Chat.tsx
+++ src/page/Chat.tsx
@@ -4,7 +4,7 @@ import { database } from "../firebase/config";
 import { get, onValue, push, ref } from "firebase/database";
 import { Divider, Grid, IconButton, InputBase, Paper } from "@mui/material";
 import ChatNode from "../Components/ChatNode";
-import AddIcon from '@mui/icons-material/Add';
+import AddIcon from "@mui/icons-material/Add";
 interface userType {
   name: string;
   uid: string;
@@ -58,42 +58,45 @@ const Chat = ({ userID, chatNode }: { userID: string; chatNode: string }) => {
             image={data.image}
           />
         ))}
+
+        <Grid item xs={12}>
+          <Paper
+            sx={{
+              p: "2px 4px",
+              display: "flex",
+              alignItems: "center",
+              bottom: 0,
+              position: "fixed",
+              width: "95%",
+            }}
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Enter your message"
+              onChange={(event) => setChatData(event.target.value)}
+              onKeyDown={(event) => {
+                if (event.key === "Enter" && chatData.trim() !== "") {
+                  push(ref(database, chatNode), {
+                    uid: userData.uid,
+                    avatar: userData.photoURL,
+                    name: userData.name,
+                    message: chatData,
+                    image: "",
+                  });
+                  setChatData("");
+                }
+              }}
+              color="success"
+              id="standard-basic"
+              value={chatData}
+            />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton color="primary" sx={{ p: "10px" }}>
+              <AddIcon />
+            </IconButton>
+          </Paper>
+        </Grid>
       </Grid>
-      <Paper
-        sx={{
-          p: "2px 4px",
-          display: "flex",
-          alignItems: "center",
-          bottom: 0,
-          position: "fixed",
-          width: "98%",
-        }}
-      >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Enter your message"
-          onChange={(event) => setChatData(event.target.value)}
-          onKeyDown={(event) => {
-            if (event.key === "Enter" && chatData.trim() !== "") {
-              push(ref(database, chatNode), {
-                uid: userData.uid,
-                avatar: userData.photoURL,
-                name: userData.name,
-                message: chatData,
-                image: "",
-              });
-              setChatData("");
-            }
-          }}
-          color="success"
-          id="standard-basic"
-          value={chatData}
-        />
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <IconButton color="primary" sx={{ p: "10px" }}>
-          <AddIcon />
-        </IconButton>
-      </Paper>
     </>
   );
 };
