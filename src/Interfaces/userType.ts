d0f9aaa8cccba7e6989fd3d3528f4cd526d8eb3c
Sayem Ahamed <95583680+Sayemahamed@users.noreply.github.com>
2024-06-25 22:27:19 +0600

    updated friends interface

--- src/Interfaces/userType.ts
+++ src/Interfaces/userType.ts
@@ -1,7 +1,11 @@
+interface friend{
+    uid: string;
+    chatNode: string;
+}
 export default interface userType {
     name: string;
     uid: string;
     photoURL: string;
     email: string;
-    friends: [{"uid": string,"chatNode": string}];
+    friends: friend[];
 }
